5b41a6c8cb19d6f9b4d86f74cc3ee416bded8d7c
Nicolas Dupont <nicolas.dupont@mesh-md.fr>
2013-10-22 12:58:07 +0200

    Initial import from Orthoplus project

---
+++ pre-commits/check_debugger.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BASEDIR="$(cd `dirname $0`; pwd)"
+
+# Checks if the given file contains the given string
+contains() {	# $1 = file path, $2 = expression to look for
+	egrep --color=auto --ignore-case -H --line-number "$2" "$1"	# -H: print filename
+}
+
+exitCode=0
+
+contains "$1" 'debugger' && exitCode=1
+
+exit $exitCode
