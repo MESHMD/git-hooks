907664cf124f7dea4680f3b27addab4033c88104
Matti Schneider <matti.schneider@mesh-md.fr>
2013-10-22 13:10:26 +0200

    Allow checking for bad words in added directories

--- pre-commits/check_debugger.sh
+++ pre-commits/check_debugger.sh
@@ -4,7 +4,7 @@ BASEDIR="$(cd `dirname $0`; pwd)"
 
 # Checks if the given file contains the given string
 contains() {	# $1 = file path, $2 = expression to look for
-	egrep --color=auto --ignore-case -H --line-number "$2" "$1"	# -H: print filename
+	egrep --recursive --color=auto --ignore-case -H --line-number "$2" "$1"	# -H: print filename
 }
 
 exitCode=0
